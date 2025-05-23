d8836bb699a44e162a661ed5e153931e11d01aa0
Carman Fu <futarimiti@163.com>
2025-05-23 19:23:03 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -1,17 +1,36 @@
 {
-  description = "dbg macro reinvention in Haskell";
+  description = "dbg macro reinvented in Haskell";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
     {
-      devShells.${system}.default = import ./shell.nix { inherit pkgs; };
-    };
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = rec {
+          dbg = import ./default.nix { inherit pkgs; };
+          default = dbg;
+        };
+        apps = rec {
+          dbg = flake-utils.lib.mkApp { drv = self.packages.${system}.dbg; };
+          default = dbg;
+        };
+        devShells.default = import ./shell.nix { inherit pkgs; };
+      }
+    );
 }
